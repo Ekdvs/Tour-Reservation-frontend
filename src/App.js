b8de50df9c50f836c202c8856d0886167135bf20
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 06:59:07 +0530

    app js add  home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+        
+        <Route exact path='/' element={<Home/>}/>
+        
+
+        </Routes>
+      </Router>
 
     </div>
   );
