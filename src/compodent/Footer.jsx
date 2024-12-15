b4873c5b02ad64cbcad15eb18fa9d5974331fc10
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 15:58:48 +0530

    add save and edit buttoon

--- src/compodent/Footer.jsx
+++ src/compodent/Footer.jsx
@@ -3,6 +3,29 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
+      <div className="container">
+            <h1 className="my-4">Profile</h1>
+
+            {/* Basic Info */}
+            <div className="card">
+                <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center mb-3">
+                        <h2>Basic Info</h2>
+                        {!isEditing ? (
+                            <button className="btn btn-link" onClick={() => setIsEditing(true)}>
+                                Edit
+                            </button>
+                        ) : (
+                            <button className="btn btn-link" onClick={handleSave}>
+                                Save
+                            </button>
+                        )}
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
       
     </div>
   )
