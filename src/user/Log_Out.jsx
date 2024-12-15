cc9c36f5dea46398c3f0e7065a18e88377925153
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 10:04:03 +0530

    update logout

--- src/user/Log_Out.jsx
+++ src/user/Log_Out.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Log_Out() {
   return (
     <div>
-      
+      <div className="logout-container">
+      <h2>Logging out...</h2>
+      <p>You are being logged out. Please wait.</p>
+    </div>
     </div>
   )
 }
