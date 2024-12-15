2326323ec5dc1ee4da4fdcf652b67496a5b01a70
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 20:38:24 +0530

    add  breadcrumb

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -16,6 +16,16 @@ export default function Travel_Booking() {
     <div>
         <Topbar/>
         <Navbar/>
+        <div className="container-fluid bg-breadcrumb">
+        <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
+          <h3 className="text-white display-3 mb-4">Online Booking</h3>
+          <ol className="breadcrumb justify-content-center mb-0">
+            <li className="breadcrumb-item"><a href="/">Home</a></li>
+            <li className="breadcrumb-item"><a href="/">Pages</a></li>
+            <li className="breadcrumb-item active text-white">Online Booking</li>
+          </ol>
+        </div>
+      </div>
 
         <Footer/>
       
