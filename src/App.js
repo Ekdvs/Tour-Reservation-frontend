1bb3ef42ee1cb7db95395f1e9a857f51517ca340
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 10:09:16 +0530

    create logi n page and connect to app js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import Register from "./user/Register";
 import Log_Out from "./user/Log_Out";
+import Login from "./user/Login";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/Register' element={<Register/>}/>
         <Route exact path='/Log_Out' element={<Log_Out/>}/>
-
+        <Route exact path='/Login' element={<Login/>}/>
         </Routes>
       </Router>
 
