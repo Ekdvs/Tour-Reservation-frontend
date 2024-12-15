9a3b95abbcfea64d8cfd98adee1501fc458f68b6
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 10:05:29 +0530

    instagram and youtube button with icon

--- src/compodent/Topbar.jsx
+++ src/compodent/Topbar.jsx
@@ -17,6 +17,12 @@ export default function Topbar() {
             <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
                 <i className="fab fa-linkedin-in fw-normal"></i>
               </a>
+              <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
+                <i className="fab fa-instagram fw-normal"></i>
+              </a>
+              <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle" href="#">
+                <i className="fab fa-youtube fw-normal"></i>
+              </a>
     </div>
   )
 }
