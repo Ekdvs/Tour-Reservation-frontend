a8c98dcf3fc4cccc5e4c518e0334b7061bc90301
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 07:16:50 +0530

    bootsttap install

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JS (includes dropdown functionality)
 import Home from "./page/Home";
 
 function App() {
