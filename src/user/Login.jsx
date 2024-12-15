f4247084a57ba5f7d9d8a1d33c3b88bfc4bf1ab6
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 10:39:20 +0530

    update breadcrumb

--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -44,7 +44,7 @@ export default function SignIn() {
       <Topbar/>
       <Navbar/>
       <div className="container-fluid bg-primary text-white text-center py-5">
-        <h3 className="display-3 mb-4">Register</h3>
+        <h3 className="display-3 mb-4">Login</h3>
         <ol className="breadcrumb justify-content-center">
           <li className="breadcrumb-item">
             <a href="/" className="text-white">Home</a>
@@ -52,7 +52,7 @@ export default function SignIn() {
           <li className="breadcrumb-item">
             <a href="/Contact" className="text-white">Pages</a>
           </li>
-          <li className="breadcrumb-item active">Register</li>
+          <li className="breadcrumb-item active">Login</li>
         </ol>
       </div>
 
