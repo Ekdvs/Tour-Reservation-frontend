9cf883d1d37d0bcba2f922db6b6906add713f248
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 20:42:40 +0530

    how mony peopls come

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -71,6 +71,16 @@ export default function Travel_Booking() {
                                         <label for="select1">Destination</label>
                                     </div>
                                 </div>
+                                <div class="col-md-6">
+                                    <div class="form-floating">
+                                        <select class="form-select bg-white border-0" id="SelectPerson">
+                                            <option value="1">Persons 1</option>
+                                            <option value="2">Persons 2</option>
+                                            <option value="3">Persons 3</option>
+                                        </select>
+                                        <label for="SelectPerson">Persons</label>
+                                    </div>
+                                </div>
 
                             </div>
                         </form>
