c2a578454c60c4d96126cff488897ef6ea807fbf
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 11:33:52 +0530

    import topbar,navbar,footer to blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import Topbar from '../components/Topbar'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 export default function Blog() {
   return (
     <div>
