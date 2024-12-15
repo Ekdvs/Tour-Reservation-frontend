604daf2be090da65fe67af9f575a11852742170f
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 09:23:54 +0530

    Implement Bootstrap Navbar with branding section

--- src/compodent/Navbar.jsx
+++ src/compodent/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar() {
     <div>
       <Header/>
       <div class="container-fluid position-relative p-0"></div>
+      <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0"></a>
     </div>
   )
 }
