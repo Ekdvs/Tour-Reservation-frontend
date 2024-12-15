2dc34919faed0a3d88cc96e348d380574a91966d
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 10:19:54 +0530

    update sign in page

--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -8,6 +8,28 @@ export default function Login() {
     <div>
       <Topbar/>
       <Navbar/>
+      <div className="container-fluid bg-primary text-white text-center py-5">
+        <h3 className="display-3 mb-4">Register</h3>
+        <ol className="breadcrumb justify-content-center">
+          <li className="breadcrumb-item">
+            <a href="/" className="text-white">Home</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="/Contact" className="text-white">Pages</a>
+          </li>
+          <li className="breadcrumb-item active">Register</li>
+        </ol>
+      </div>
+
+      <div className="container my-5">
+        {message && <div className={message.class}>{message.text}</div>}
+        <div className="card mx-auto" style={{ maxWidth: '500px' }}>
+          <div className="card-body">
+            <h5 className="card-title text-center">Sign IN</h5>
+            
+          </div>
+        </div>
+      </div>
 
 
       <Footer/>
