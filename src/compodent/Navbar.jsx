b2f90307a5ee01579f9998ecfbdd8f92e894bef7
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 09:46:07 +0530

    add book now link

--- src/compodent/Navbar.jsx
+++ src/compodent/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import Header from './Header'
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const ActiveNavLink = styled(NavLink)`
+  color: #13357B;
+  font-weight: bold;
+`;
 export default function Navbar() {
   return (
     <div>
       <Header/>
-      <div class="container-fluid position-relative p-0"></div>
-      <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+        <div class="container-fluid position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                 <a href="" class="navbar-brand p-0">
-                <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>Ceylon <br></br>Paradice</h1>
+                    <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>Ceylon <br></br>Paradice</h1>
+
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
@@ -19,7 +29,7 @@ export default function Navbar() {
                     <ActiveNavLink to="/Services" className="nav-item nav-link">Services</ActiveNavLink>
                     <ActiveNavLink to="/Packages" className="nav-item nav-link">Packages</ActiveNavLink>
                     <ActiveNavLink to="/Blog" className="nav-item nav-link">Blog</ActiveNavLink>
-                    <div class="nav-item dropdown">
+                        <div class="nav-item dropdown">
                             <a href="/" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu m-0">
                                 <a href="/Destination" class="dropdown-item">Destination</a>
@@ -35,7 +45,9 @@ export default function Navbar() {
                     </div>
                     <a href="/Travel_Booking" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Book Now</a>
                 </div>
-      </nav>
-    </div>
-  )
+            </nav>
+            </div>
+            </div>
+            
+  )
 }
