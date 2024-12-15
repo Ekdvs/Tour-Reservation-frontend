f4bcccd357523a026344cc2b1c4586fe3fee9fa5
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-15 12:36:22 +0530

    classname

--- src/compodent/Footer.jsx
+++ src/compodent/Footer.jsx
@@ -11,10 +11,10 @@ export default function Footer() {
       <div className="col-md-6 col-lg-6 col-xl-3">
       <div className="footer-item d-flex flex-column">
         <h4 className="mb-4 text-white">Get In Touch</h4>
-        <a href=""><i class="fas fa-home me-2"></i>123 Kandy Colombo Main Street, Kelaniya, Sri Lanka </a>
-        <a href=""> <i class="fas fa-envelope me-2"></i> info@example.com</a>
-        <a href=""><i class="fas fa-phone me-2"></i> +9471 897 4153</a>
-        <a href="" class="mb-3"><i class="fas fa-print me-2"></i> +9477 326 0660</a>
+        <a href=""><i className="fas fa-home me-2"></i>123 Kandy Colombo Main Street, Kelaniya, Sri Lanka </a>
+        <a href=""> <i className="fas fa-envelope me-2"></i> info@example.com</a>
+        <a href=""><i className="fas fa-phone me-2"></i> +9471 897 4153</a>
+        <a href="" className="mb-3"><i className="fas fa-print me-2"></i> +9477 326 0660</a>
         <div className="d-flex align-items-center">
 
         </div>
