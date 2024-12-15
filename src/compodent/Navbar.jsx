4a5f34ad597049937e8a43657b0f5cddb2f5e50a
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 09:25:34 +0530

    add heading

--- src/compodent/Navbar.jsx
+++ src/compodent/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar() {
       <div class="container-fluid position-relative p-0"></div>
       <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                 <a href="" class="navbar-brand p-0"></a>
+                <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>Ceylon <br></br>Paradice</h1>
     </div>
   )
 }
