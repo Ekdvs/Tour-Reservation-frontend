f4c862fa779d9f60329a82be9364ab893a3f19b1
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 13:20:13 +0530

    Online Payment heading to CardPayment component

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -11,10 +11,14 @@ export default function CardPayment() {
   const handlePayment = (e) => {
     e.preventDefault();
     alert("Payment Successful!");
+  };
   return (
     <div>
       <Topbar/>
       <Navbar/>
+      <div className="container-fluid bg-breadcrumb">
+        <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
+          <h3 className="text-white display-3 mb-4">Online Payment</h3>
     </div>
   )
 }
