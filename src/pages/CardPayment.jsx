7ce0de4b1081a218ee8577fde79f7619c157d374
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 13:50:16 +0530

    card holder name input

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -141,6 +141,10 @@ export default function CardPayment() {
                     </div>
                   </div>
                 </div>
+                <div className="form-group">
+                  <label htmlFor="card-holder" className="control-label">
+                    CARD HOLDER NAME
+                  </label>
     </div>
   )
 }
