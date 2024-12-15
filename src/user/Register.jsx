b403f94fd35041bc6094016a86d6d0681c5f82a8
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 09:43:25 +0530

    add breadcrumb

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -9,6 +9,17 @@ export default function Register() {
       <Topbar/>
       <Navbar/>
 
+      <div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style={{ maxWidth: '900px' }}>
+                <h3 class="text-white display-3 mb-4">Our Services</h3>
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/Services">Pages</a></li>
+                    <li class="breadcrumb-item active text-white">Services</li>
+                </ol>    
+            </div>
+        </div>
+
       <Footer/>
 
     </>
