42bf376073de073a828fb07d31f58f6920d706ca
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 20:36:25 +0530

    add to card payment link to booking

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function Travel_Booking() {
+    const navigate = useNavigate();
+
+  const handleBooking = (e) => {
+    e.preventDefault();
+    navigate("/Cardpayment");
+  };
+
   return (
     <div>
       
