05e43169154b8fb91b4071ec7282e7203a72fdc6
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 09:52:38 +0530

    Add responsive container section with background and alignment styles

--- src/compodent/Topbar.jsx
+++ src/compodent/Topbar.jsx
@@ -4,6 +4,10 @@ export default function Topbar() {
   return (
     <div>
       <Header/>
+      <div className="container-fluid bg-primary px-5 d-none d-lg-block">
+        <div className="row gx-0">
+          <div className="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
+            <div className="d-inline-flex align-items-center" style={{ height: '45px' }}></div>
     </div>
   )
 }
