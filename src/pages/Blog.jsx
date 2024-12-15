e9c3d2e837c992080bbaa4fc23783e1a489bfbb9
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 11:38:25 +0530

    create heading to blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,7 +5,12 @@ import Footer from '../components/Footer'
 export default function Blog() {
   return (
     <div>
-      
+     <Topbar/>
+      <Navbar/>
+     
+        <div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style={{ maxWidth: '900px' }}>
+                <h3 class="text-white display-3 mb-4">Our Blog</h3> 
     </div>
   )
 }
