c9b35cf5a2107b3b118bc75b3db9933f9454ec1c
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 10:02:49 +0530

    Facebook button with icon

--- src/compodent/Topbar.jsx
+++ src/compodent/Topbar.jsx
@@ -11,6 +11,9 @@ export default function Topbar() {
             <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"  href="https://twitter.com" target="_blank" >
                 <i className="fab fa-twitter fw-normal"></i>
             </a>
+            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
+                <i className="fab fa-facebook-f fw-normal"></i>
+            </a>
     </div>
   )
 }
