f977a56184de2c483e6bed2d1177f44f33700810
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2024-12-15 13:16:41 +0530

    Initialize cardNumber state in CardPayment component

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function CardPayment() {
+  const [cardNumber, setCardNumber] = useState("");
   return (
     <div>
       <Topbar/>
