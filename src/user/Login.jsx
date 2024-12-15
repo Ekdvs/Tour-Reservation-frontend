c5be4571ae7f0a7df2cfa2083115de5a0df9d436
Ekdvs <ekdvsampath02@gmail.com>
2024-12-15 10:24:54 +0530

    login to change to SignIn

--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -3,7 +3,7 @@ import Topbar from '../compodent/Topbar'
 import Navbar from '../compodent/Navbar'
 import Footer from '../compodent/Footer'
 
-export default function Login() {
+export default function SignIn() {
   return (
     <div>
       <Topbar/>
