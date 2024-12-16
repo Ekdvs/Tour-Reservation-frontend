fb13cfa3b40e43af1d936f33d950053aaee9ffe6
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 08:25:27 +0530

    add Travel Guides

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -52,6 +52,19 @@ export default function Services() {
                                     </div>
                                 </div>
                             </div> 
+                            <div class="col-12">
+                                <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
+                                    <div class="service-content text-end">
+                                        <h5 class="mb-4">Travel Guides</h5>
+                                        <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
+                                        </p>
+                                    </div>
+                                    <div class="service-icon p-4">
+                                        <i class="fa fa-user fa-4x text-primary"></i>
+                                    </div>
+                                </div>
+                            </div>
+
 
                         </div>
                     </div>
