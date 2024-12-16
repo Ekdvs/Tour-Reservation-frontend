77c0f30d522ec3c9971f9f96abdfe8a78da74f29
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 08:23:03 +0530

    fix service

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -18,6 +18,27 @@ export default function Services() {
                 </ol>    
             </div>
         </div>
+        <div class="container-fluid bg-light service py-5">
+            <div class="container py-5">
+                <div class="mx-auto text-center mb-5" style={{ maxWidth: '900px' }}>
+                    <h5 class="section-title px-3">Searvices</h5>
+                    <h1 class="mb-0">Our Services</h1>
+                </div>
+                <div class="row g-4">
+                    <div class="col-lg-6">
+                        <div class="row g-4">
+                            
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="text-center">
+                            <a class="btn btn-primary rounded-pill py-3 px-5 mt-2" href="">Service More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
 
       <Footer/>
