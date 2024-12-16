9e969af8036c612434812665c1365cdd471d1461
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 08:12:55 +0530

    add topbar

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Topbar from '../components/Topbar'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Subscribe from './Subscribe'
+
 
 export default function Contact() {
   return (
     <div>
+      <Topbar/>
+      <Navbar/>
       
     </div>
   )
