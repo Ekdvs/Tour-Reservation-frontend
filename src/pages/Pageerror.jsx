35e01569e83c3dff28e6532f42cf485a5e0da9d1
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 08:32:49 +0530

    add foter

--- src/pages/Pageerror.jsx
+++ src/pages/Pageerror.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Topbar from '../compodent/Topbar'
+import Footer from '../compodent/Footer'
 
 export default function Pageerror() {
   return (
     <div>
         <Topbar/>
         <Navbar/>
-        
+
+        <Footer/>
+
 
       
     </div>
