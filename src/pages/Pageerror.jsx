d86f71db6cae4a365bbb3dae6f7762a674b654f0
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2024-12-16 08:32:14 +0530

    add top bar

--- src/pages/Pageerror.jsx
+++ src/pages/Pageerror.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Topbar from '../compodent/Topbar'
 
 export default function Pageerror() {
   return (
     <div>
+        <Topbar/>
+        
       
     </div>
   )
