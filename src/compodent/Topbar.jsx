28037d5f66ca7bdaa4c5f5196f314784785e38e3
Ekdvs <ekdvsampath02@gmail.com>
2024-12-27 12:10:46 +0530

    add social media link

--- src/compodent/Topbar.jsx
+++ src/compodent/Topbar.jsx
@@ -5,7 +5,32 @@ import Header from "./Header";
 export default function Topbar() {
     return (
         <div>
-            <Header />
+            <Header/>
+            <div className="container-fluid bg-primary px-5 d-none d-lg-block">
+                <div className="row gx-0">
+                    <div className="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
+                        <div className="d-inline-flex align-items-center" style={{height: '45px'}}>
+                            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2"
+                               href="https://twitter.com" target="_blank">
+                                <i className="fab fa-twitter fw-normal"></i>
+                            </a>
+                            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
+                                <i className="fab fa-facebook-f fw-normal"></i>
+                            </a>
+                            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
+                                <i className="fab fa-linkedin-in fw-normal"></i>
+                            </a>
+                            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href="#">
+                                <i className="fab fa-instagram fw-normal"></i>
+                            </a>
+                            <a className="btn btn-sm btn-outline-light btn-sm-square rounded-circle" href="#">
+                                <i className="fab fa-youtube fw-normal"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     )
