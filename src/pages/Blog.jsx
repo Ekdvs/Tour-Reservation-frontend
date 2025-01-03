8dc600a93fa02c44cd9eb9edad8fccb6feb9d576
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:45:48 +0530

    add new blog item component with image display

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -50,3 +50,9 @@ export default function Blog() {
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="blog-item">
+                            <div class="blog-img">
+                                <div class="blog-img-inner">
+                                    <img class="img-fluid w-100 rounded-top" src="img/blog-2.jpg" alt="Image"/>
+                                    
