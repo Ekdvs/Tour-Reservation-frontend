69e69b7a1158bd96af5e9d6d4d70fc3e8f811f3b
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:59:39 +0530

    add Footer component and useNavigate hook to Travel Booking page

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -1,3 +1,5 @@
 import React from "react";
 import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
+import Footer from "../compodent/Footer";
+import {useNavigate} from 'react-router-dom';
