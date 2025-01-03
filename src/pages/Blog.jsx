967cec36aa7070b24a080e928eb8a4e18fe2e516
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:48:53 +0530

    add comment count to blog info section

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -85,4 +85,6 @@ export default function Blog() {
                                 <div class="blog-info d-flex align-items-center border border-start-0 border-end-0">
                                     <small class="flex-fill text-center border-end py-2"><i class="fa fa-calendar-alt text-primary me-2"></i>28 Jan 2050</small>
                                     <a href="#" class="btn-hover flex-fill text-center text-white border-end py-2"><i class="fa fa-thumbs-up text-primary me-2"></i>1.7K</a>
-                                    
+                                    <a href="#" class="btn-hover flex-fill text-center text-white py-2"><i class="fa fa-comments text-primary me-2"></i>1K</a>
+                                </div>
+                            </div>
