410ac0f92808bf5d287f4c26adb03dfd167fa796
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 00:56:40 +0530

    add About Us heading to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,4 +10,5 @@ export default function About() {
             <Navbar/>
             <div className="container-fluid bg-breadcrumb">
             <div className="container text-center py-5" style={{ maxWidth: '900px' }}>
-                
+            <h3 className="text-white display-3 mb-4">About Us</h3>
+            
