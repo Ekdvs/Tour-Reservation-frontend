577ba516abef8eff579b91d6dcc2ed96ee57d0c4
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:10:11 +0530

    add search input and button to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,6 +67,14 @@ export default function Home() {
                 </div>
                 
         <div className="container-fluid search-bar position-relative" style={{ top: '-50%', transform: 'translateY(-50%)' }}>
-
-            
-                                    
+            <div className="container">
+                <div className="position-relative rounded-pill w-100 mx-auto p-5" style={{ background: 'rgba(19, 53, 123, 0.8)' }}                >
+                    <input className="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Eg: Sri lanka"/>
+                    <button type="button" className="btn btn-primary rounded-pill py-2 px-4 position-absolute me-2" style={{ top: '50%', right: '46px', transform: 'translateY(-50%)' }}
+                    >Search</button>
+                </div>
+            </div>
+        </div>
+        <div>
+        
+        </div>
