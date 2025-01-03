49e4aaafa3b90650e9c8b3ae71c1a6e863a3c48a
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 01:58:42 +0530

    add Subscribe component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,3 +2,6 @@ import React from "react";
 import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
 import Footer from "../compodent/Footer";
+import Subscribe from './Subscribe'
+
+export default function Home() {
