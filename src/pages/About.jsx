2407fa762a3c65336869ad23c649004db56badc0
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 01:03:30 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,4 +11,10 @@ export default function About() {
             <div className="container-fluid bg-breadcrumb">
             <div className="container text-center py-5" style={{ maxWidth: '900px' }}>
             <h3 className="text-white display-3 mb-4">About Us</h3>
-            
+            <ol className="breadcrumb justify-content-center mb-0">
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item"><a href="/About">Pages</a></li>
+                    <li className="breadcrumb-item active text-white">About</li>
+                </ol>
+            </div>
+        </div>
