e5555045141d290da089eb535503b03849e45c64
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 01:09:23 +0530

    fix: ensure proper formatting and structure in About page component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,17 +4,20 @@ import Navbar from '../compodent/Navbar'
 import Footer from '../compodent/Footer'
 
 export default function About() {
-    return (
-        <div>
-            <Topbar/>
-            <Navbar/>
-            <div className="container-fluid bg-breadcrumb">
+  return (
+    <div>
+        <Topbar/>
+        <Navbar/>
+        <div className="container-fluid bg-breadcrumb">
             <div className="container text-center py-5" style={{ maxWidth: '900px' }}>
-            <h3 className="text-white display-3 mb-4">About Us</h3>
-            <ol className="breadcrumb justify-content-center mb-0">
+                <h3 className="text-white display-3 mb-4">About Us</h3>
+                <ol className="breadcrumb justify-content-center mb-0">
                     <li className="breadcrumb-item"><a href="/">Home</a></li>
                     <li className="breadcrumb-item"><a href="/About">Pages</a></li>
                     <li className="breadcrumb-item active text-white">About</li>
                 </ol>
             </div>
         </div>
+    </div>
+    )
+}
