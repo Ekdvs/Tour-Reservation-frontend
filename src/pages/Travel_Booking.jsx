9d53a1f1d1ec1586896755dce0251921f1da88a3
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 03:10:44 +0530

    add breadcrumb navigation to Travel Booking page

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -17,4 +17,11 @@ export default function Travel_Booking() {
             <div className="container-fluid bg-breadcrumb">
             <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
                 <h3 className="text-white display-3 mb-4">Online Booking</h3>
-          
+                <ol className="breadcrumb justify-content-center mb-0">
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item"><a href="/">Pages</a></li>
+                    <li className="breadcrumb-item active text-white">Online Booking</li>
+                </ol>
+            </div>
+            </div>
+            
