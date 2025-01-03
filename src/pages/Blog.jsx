69afdec5893b19a200445a58468e0c4a47466eb6
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:41:55 +0530

    add breadcrumb navigation to Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,4 +11,10 @@ export default function Blog() {
         <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style={{ maxWidth: '900px' }}>
                 <h3 class="text-white display-3 mb-4">Our Blog</h3> 
-                
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/Blog">Pages</a></li>
+                    <li class="breadcrumb-item active text-white">Blog</li>
+                </ol>
+            </div>
+        </div>
