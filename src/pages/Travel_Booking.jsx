4acb4627d928b1d703b92fd864c46524fe92b4df
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 03:05:54 +0530

    add useNavigate hook and export Travel_Booking component

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -3,3 +3,6 @@ import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
 import Footer from "../compodent/Footer";
 import {useNavigate} from 'react-router-dom';
+
+export default function Travel_Booking() {
+    const navigate = useNavigate();
