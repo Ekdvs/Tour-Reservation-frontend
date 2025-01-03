94b4227108a48269f119b54c57e321797cb1e2a1
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 03:06:22 +0530

    add handleBooking function to navigate to Cardpayment page

--- src/pages/Travel_Booking.jsx
+++ src/pages/Travel_Booking.jsx
@@ -6,3 +6,7 @@ import {useNavigate} from 'react-router-dom';
 
 export default function Travel_Booking() {
     const navigate = useNavigate();
+    const handleBooking = (e) => {
+        e.preventDefault();
+        navigate("/Cardpayment");
+      };
