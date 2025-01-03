27539e895b7e6bd632c39aa3b1c0c5c0ba10f634
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 02:40:59 +0530

    add Blog component with Topbar and Navbar

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,3 +3,8 @@ import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
 import Footer from "../compodent/Footer";
 
+export default function Blog() {
+    return (
+        <div>
+        <Topbar/>
+        <Navbar/>
