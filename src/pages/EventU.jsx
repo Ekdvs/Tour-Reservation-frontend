ed7087fa650bc73e01dfcddd9de7e897a6474974
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 12:35:49 +0530

    add fetchEvents function to EventShowPage for retrieving events

--- src/pages/EventU.jsx
+++ src/pages/EventU.jsx
@@ -5,3 +5,5 @@ import "./EventShowPage.css"; // Custom CSS file for styling
 const EventShowPage = () => {
     const [events, setEvents] = useState([]);
     const API_BASE_URL = "http://localhost:8080/event";
+     // Fetch all events
+    const fetchEvents = async () => {
