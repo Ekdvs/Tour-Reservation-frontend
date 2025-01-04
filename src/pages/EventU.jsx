60af9f4e68185e568f4c99bb6b50efcaa8aea74f
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 14:11:28 +0530

    add useState and useEffect imports in CardPayment and EventU components

--- src/pages/EventU.jsx
+++ src/pages/EventU.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./EventShowPage.css"; // Custom CSS file for styling
