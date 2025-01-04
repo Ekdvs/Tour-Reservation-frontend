7021f8544dc8e874d6d5eaa806f157722ad280b7
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 12:28:19 +0530

    add newsletter title and description to Subscribe component

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -6,7 +6,9 @@ export default function Subscribe() {
             <div className="container text-center py-5">
             <div className="mx-auto text-center" style={{maxwidth: "900px"}}>
                 <h5 className="subscribe-title px-3">Subscribe</h5>
-                
+                <h1 className="text-white mb-4">Our Newsletter</h1>
+                    <p className="text-white mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempore nam, architecto doloremque velit explicabo? Voluptate sunt eveniet fuga eligendi! Expedita laudantium fugiat corrupti eum cum repellat a laborum quasi.
+                    </p>
         </div>
     )
 }
