eab0c8f0c8483d9dc10568938640639946896c11
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 12:27:04 +0530

    add layout structure to Subscribe component

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -2,6 +2,9 @@ import React from "react";
 export default function Subscribe() {
     return (
         <div>
+            <div className="container-fluid subscribe py-5">
+            <div className="container text-center py-5">
+            
         </div>
     )
 }
