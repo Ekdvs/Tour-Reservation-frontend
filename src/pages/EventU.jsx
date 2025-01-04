0fed507289d71abb305a6c45f99876349b791cf0
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 12:35:06 +0530

    add EventShowPage component with initial state and API URL

--- src/pages/EventU.jsx
+++ src/pages/EventU.jsx
@@ -2,3 +2,6 @@ import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./EventShowPage.css"; // Custom CSS file for styling
+const EventShowPage = () => {
+    const [events, setEvents] = useState([]);
+    const API_BASE_URL = "http://localhost:8080/event";
