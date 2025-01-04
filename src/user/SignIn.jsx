2d63edb4c0ec3be570de5570388932c77f87a1ac
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 10:46:19 +0530

    feat: add useNavigate hook to SignIn component

--- src/user/SignIn.jsx
+++ src/user/SignIn.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Topbar from '../compodent/Topbar'
 import Navbar from '../compodent/Navbar'
 import Footer from '../compodent/Footer'
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
   const [userEmail, setUserEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [showPassword, setShowPassword] = useState(false);
- 
+  const navigate = useNavigate();
   return (
     <div>
       <Topbar/>
