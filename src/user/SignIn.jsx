af288eeb81d719b5ab44cac8adad80fcfd798249
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 10:40:06 +0530

    feat: add breadcrumb navigation to SignIn page

--- src/user/SignIn.jsx
+++ src/user/SignIn.jsx
@@ -8,7 +8,16 @@ export default function SignIn() {
     <div>
       <Topbar/>
       <Navbar/>
-
+      <div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style={{maxWidth:"900px"}}>
+                <h3 class="text-white display-3 mb-4">Login</h3>
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/Contact">Pages</a></li>
+                    <li class="breadcrumb-item active text-white">Login</li>
+                </ol>    
+            </div>
+        </div>
       <Footer/>
       
     </div>
