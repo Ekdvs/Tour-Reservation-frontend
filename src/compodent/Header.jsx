972343f898c8980ddf87aca5eeaf401fbc67c41c
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:10:30 +0530

    add preconnect link for Google Fonts CDN in Header component

--- src/compodent/Header.jsx
+++ src/compodent/Header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
             <meta charSet="utf-8"></meta>
             <title>Travela - Tourism Website Template</title>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             
             </head>
 
