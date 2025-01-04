7ecb3512bb9e02543125cf0d7a65c83347becbf1
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 11:38:03 +0530

    add card number state to CardPayment component

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -3,3 +3,5 @@ import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
 import Footer from "../compodent/Footer";
 
+export default function CardPayment() {
+    const [cardNumber, setCardNumber] = useState("");
