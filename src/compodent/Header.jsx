749041c4b6a9532e21fc6696f5f8a654739332a3
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:09:07 +0530

    remove unnecessary meta and link tags from Header component

--- src/compodent/Header.jsx
+++ src/compodent/Header.jsx
@@ -4,31 +4,7 @@ export default function Header() {
     return (
         <div>
             <head>
-                <meta charSet="utf-8"></meta>
-                <title>Travela - Tourism Website Template</title>
-                <link rel="stylesheet" href="admin/assets/css/bootstrap.min.css"/>
-                <link rel="stylesheet" href="admin/assets/css/plugins.min.css"/>
-                <link rel="stylesheet" href="admin/assets/css/kaiadmin.min.css"/>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap"
-                      rel="stylesheet"/>
-
-                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
-                      rel="stylesheet"/>
-
-
-                <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"/>
-                <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet"/>
-
-
-                <link href="css/bootstrap.min.css" rel="stylesheet"/>
-
-
-                <link href="css/style.css" rel="stylesheet"/>
-                <link href="css/payment.css" rel="stylesheet"></link>
-                <link href="css/ForgotPassword.css" rel="stylesheet"></link>
+                
 
 
             </head>
