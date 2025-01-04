5b5e5da740bc014428b1af32e722461318fb5273
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:11:17 +0530

    add Google Fonts stylesheet link to Header component

--- src/compodent/Header.jsx
+++ src/compodent/Header.jsx
@@ -8,7 +8,9 @@ export default function Header() {
             <title>Travela - Tourism Website Template</title>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-            
+            <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet"/> 
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        
             </head>
 
         </div>
