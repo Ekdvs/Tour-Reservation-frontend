46f5587fa33554892c4339559561dcede0bb5abd
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:14:52 +0530

    add additional CSS links for styling in Headeradmin component

--- src/compodent/Headeradmin.jsx
+++ src/compodent/Headeradmin.jsx
@@ -4,7 +4,11 @@ export default function Headeradmin() {
   return (
     <div>
         <head>
-            
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="stylesheet" href="admin/assets/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="admin/assets/css/plugins.min.css" />
+            <link rel="stylesheet" href="admin/assets/css/kaiadmin.min.css" />
+        
         </head>
       
     </div>
