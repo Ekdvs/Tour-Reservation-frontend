295142a565c6e6af2d3d7c27a47dd4fec928d1b1
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 11:52:44 +0530

    feat: add breadcrumb navigation to VerifyOTP component for improved user experience

--- src/user/VerifyOTP.jsx
+++ src/user/VerifyOTP.jsx
@@ -8,6 +8,17 @@ export default function VerifyOTP() {
     <div>
       <Topbar/>
       <Navbar/>
+      <div class="container-fluid bg-breadcrumb">
+            <div class="container text-center py-5" style={{maxWidth:"900px"}}>
+                <h3 class="text-white display-3 mb-4">Login</h3>
+                <ol className="breadcrumb justify-content-center mb-0">
+                        <li className="breadcrumb-item"><a href="/">Home</a></li>
+                        <li className="breadcrumb-item"><a href="/Contact">Pages</a></li>
+                        <li className="breadcrumb-item active text-white">Password Reset</li>
+                        <li className="breadcrumb-item active text-white">Verify OTP</li>
+                    </ol>   
+            </div>
+        </div>
 
       <Footer/>
     </div>
