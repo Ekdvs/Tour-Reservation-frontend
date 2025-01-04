c3c716cbcb28a754956ca68c37c0a8cb4878b109
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 15:04:38 +0530

    add breadcrumb navigation to Travel Guides page

--- src/pages/Travel_Guides.jsx
+++ src/pages/Travel_Guides.jsx
@@ -11,4 +11,6 @@ export default function Travel_Guides() {
             <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style={{maxwidth: "900px"}}>
                 <h3 class="text-white display-3 mb-4">Our Travel Guides</h3>
-                
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    
