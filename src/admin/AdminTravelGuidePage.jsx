11fce295dadfb8683cfecda93571e42b7839aafb
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-04 21:12:20 +0530

    Modal for viewing travel guide history

--- src/admin/AdminTravelGuidePage.jsx
+++ src/admin/AdminTravelGuidePage.jsx
@@ -1,9 +1,19 @@
+import { Modal } from 'bootstrap';
 import React from 'react'
 
 export default function AdminTravelGuidePage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+
+    // Modal for viewing travel guide history
+    const UserHistoryModal = ({ show, user, onClose }) => (
+        <Modal show={show} onHide={onClose} size="lg">
+    <Modal.Header closeButton>
+      <Modal.Title>Travel Guide History</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        
+    </Modal.Body>
+    </Modal>
+
+    )
 }
