1f80d466451309704dbb49921c1c48c1c337c60e
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 10:31:24 +0530

    feat: add logging out message to LogOut component

--- src/user/LogOut.jsx
+++ src/user/LogOut.jsx
@@ -4,7 +4,8 @@ export default function LogOut() {
   return (
     <div>
       <div className="logout-container">
-      
+      <h2>Logging out...</h2>
+      <p>You are being logged out. Please wait.</p>
     </div>
     </div>
   )
