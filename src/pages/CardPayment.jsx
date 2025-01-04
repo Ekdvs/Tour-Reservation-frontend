459bc085a41cec1c086f2e536fa1f9f8cdffcaa5
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 11:40:54 +0530

    add breadcrumb section to CardPayment page

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -16,3 +16,7 @@ export default function CardPayment() {
         <div>
             <Topbar/>
             <Navbar/>
+            <div className="container-fluid bg-breadcrumb">
+            <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
+                <h3 className="text-white display-3 mb-4">Online Payment</h3>
+                
