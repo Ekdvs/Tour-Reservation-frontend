e643ed63426b48c546bf679354db3a7a45e671c3
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 11:28:39 +0530

    feat: add email input field to ForgotPassword component for password recovery

--- src/user/ForgotPassword.jsx
+++ src/user/ForgotPassword.jsx
@@ -32,7 +32,16 @@ export default function ForgotPassword() {
                                         <label htmlFor="email" className="form-label" style={{ fontSize: '18px', color: 'black', fontWeight: 'normal' }}>
                                             Email:
                                         </label>
-                                        
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            aria-label="Email address for password recovery"
+                                            placeholder="name@example.com"
+                                            value={userEmail}
+                                            onChange={(e) => setUserEmail(e.target.value)}
+                                            required
+                                        />
                                     </div>
                                     <div className="d-grid">
                                         <button type="submit" className="btn btn-lg btn-primary">
