af089fccf033e85f8ec47213e86e86efec9b4202
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:38:44 +0530

    add Navbar and Footer components to Register; include breadcrumb navigation

--- src/user/Register.jsx
+++ src/user/Register.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Topbar from '../compodent/Topbar'
+import Navbar from '../compodent/Navbar'
+import Footer from '../compodent/Footer'
 
 export default function Register() {
   return (
     <div>
         <Topbar/>
+        <Navbar/>
+        <div class="container-fluid bg-breadcrumb">
+                  <div class="container text-center py-5" style={{maxWidth:"900px"}}>
+                      <h3 class="text-white display-3 mb-4">Register</h3>
+                      <ol class="breadcrumb justify-content-center mb-0">
+                          <li class="breadcrumb-item"><a href="/">Home</a></li>
+                          <li class="breadcrumb-item"><a href="/Register">Pages</a></li>
+                          <li class="breadcrumb-item active text-white">Register</li>
+                      </ol>    
+                  </div>
+            </div>
+
+        <Footer/>
+
       
     </div>
   )
