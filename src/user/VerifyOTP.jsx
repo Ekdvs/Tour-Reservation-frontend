78d39f07bd21acdf724169703a63ecfea7e92659
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 11:53:26 +0530

    fix: correct indentation in breadcrumb navigation of VerifyOTP component

--- src/user/VerifyOTP.jsx
+++ src/user/VerifyOTP.jsx
@@ -12,11 +12,11 @@ export default function VerifyOTP() {
             <div class="container text-center py-5" style={{maxWidth:"900px"}}>
                 <h3 class="text-white display-3 mb-4">Login</h3>
                 <ol className="breadcrumb justify-content-center mb-0">
-                        <li className="breadcrumb-item"><a href="/">Home</a></li>
-                        <li className="breadcrumb-item"><a href="/Contact">Pages</a></li>
-                        <li className="breadcrumb-item active text-white">Password Reset</li>
-                        <li className="breadcrumb-item active text-white">Verify OTP</li>
-                    </ol>   
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item"><a href="/Contact">Pages</a></li>
+                    <li className="breadcrumb-item active text-white">Password Reset</li>
+                    <li className="breadcrumb-item active text-white">Verify OTP</li>
+                </ol>   
             </div>
         </div>
 
