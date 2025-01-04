3ffc7c27e1b621a2a45f09208d720bca5313613c
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 11:41:57 +0530

    add breadcrumb navigation to CardPayment page

--- src/pages/CardPayment.jsx
+++ src/pages/CardPayment.jsx
@@ -19,4 +19,11 @@ export default function CardPayment() {
             <div className="container-fluid bg-breadcrumb">
             <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
                 <h3 className="text-white display-3 mb-4">Online Payment</h3>
-                
+                <ol className="breadcrumb justify-content-center mb-0">
+                <li className="breadcrumb-item"><a href="/">Home</a></li>
+                <li className="breadcrumb-item"><a href="/Travel_Booking">Pages</a></li>
+                <li className="breadcrumb-item active text-white">Online Booking</li>
+                </ol>
+            </div>
+            </div>
+            
