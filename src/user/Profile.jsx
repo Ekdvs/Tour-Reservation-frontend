a7b86cac1528921727ab33f468b759533f1cf30d
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 13:44:10 +0530

    feat: add Navbar component to Profile

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Topbar from '../compodent/Topbar'
+import Navbar from '../compodent/Navbar'
 
 export default function Profile() {
   return (
     <div>
       <Topbar/>
+      <Navbar/>
     </div>
   )
 }
