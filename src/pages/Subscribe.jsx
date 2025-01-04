14fb6f09eb0c06707450fe59be8eeb30044d7d88
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-04 12:29:30 +0530

    add subscribe button to Subscribe component

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -11,7 +11,11 @@ export default function Subscribe() {
                     </p>
                     <div className="position-relative mx-auto">
                     <input className="form-control border-primary rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email"/>
-                    
+                    <button type="button" className="btn btn-primary rounded-pill position-absolute top-0 end-0 py-2 px-4 mt-2 me-2">Subscribe</button>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
