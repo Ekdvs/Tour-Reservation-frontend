658b85beeb1755cbf798e7d5aa39599ef4f2dd5f
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 09:09:51 +0530

    add meta charset and title to Header component

--- src/compodent/Header.jsx
+++ src/compodent/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
     return (
         <div>
             <head>
-                
-
-
+            <meta charSet="utf-8"></meta>
+            <title>Travela - Tourism Website Template</title>
+            
             </head>
 
         </div>
