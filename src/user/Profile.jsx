2ce9c2382511e8a7589ce15d1bbaf90c9fe54322
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 13:44:28 +0530

    feat: add Footer component to Profile

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Topbar from '../compodent/Topbar'
 import Navbar from '../compodent/Navbar'
+import Footer from '../compodent/Footer'
 
 export default function Profile() {
   return (
     <div>
       <Topbar/>
       <Navbar/>
+
+      <Footer/>
     </div>
   )
 }
