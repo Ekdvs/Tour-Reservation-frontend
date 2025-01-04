6b7e0434bd30d8322f218ca313e611366880ef62
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 13:45:06 +0530

    feat: add breadcrumb navigation to Profile component

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -8,7 +8,16 @@ export default function Profile() {
     <div>
       <Topbar/>
       <Navbar/>
-
+      <div className="container-fluid bg-breadcrumb">
+            <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
+                    <h3 className="text-white display-3 mb-4">My Profile</h3>
+                    <ol className="breadcrumb justify-content-center mb-0">
+                        <li className="breadcrumb-item"><a href="/">Home</a></li>
+                        <li className="breadcrumb-item"><a href="/Contact">Pages</a></li>
+                        <li className="breadcrumb-item active text-white">My Profile</li>
+                    </ol>
+                </div>
+         </div>
       <Footer/>
     </div>
   )
