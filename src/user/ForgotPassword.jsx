112a015645eb440e58732124e8dbdf1c74006979
Ekdvs <ekdvsampath02@gmail.com>
2025-01-04 11:04:41 +0530

    feat: add Topbar, Navbar, and Footer components to ForgotPassword page

--- src/user/ForgotPassword.jsx
+++ src/user/ForgotPassword.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Topbar from '../compodent/Topbar'
+import Navbar from '../compodent/Navbar'
+import Footer from '../compodent/Footer'
 
 export default function ForgotPassword() {
   return (
     <div>
+    <Topbar/>
+    <Navbar/>
+
+
+    <Footer/>
       
     </div>
   )
