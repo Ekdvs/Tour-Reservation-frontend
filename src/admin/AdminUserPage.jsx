53bca59692933cdfa8e6cc9e86d7d22b0cc3b283
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-05 20:26:57 +0530

    Modal header

--- src/admin/AdminUserPage.jsx
+++ src/admin/AdminUserPage.jsx
@@ -5,4 +5,7 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:808
 // Modal for viewing user history
 const UserHistoryModal = ({ show, user, onClose }) => (
 <Modal show={show} onHide={onClose} size="lg">
+<Modal.Header closeButton>
+      <Modal.Title>User History</Modal.Title>
+    </Modal.Header>
 
