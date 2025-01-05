2f4658dca0d121b998c019eee0eb482803069359
Ekdvs <ekdvsampath02@gmail.com>
2025-01-05 08:54:15 +0530

    feat: add Navbar component to Services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Topbar from "../compodent/Topbar";
+import Navbar from "../compodent/Navbar";
 
 export default function Services() {
     return (
         <div>
         <Topbar/>
+        <Navbar/>
         </div>
     )
 }
