022c816b12861c689910f6b61938e25f80c833ae
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-05 20:25:52 +0530

    const UserHistoryModal

--- src/admin/AdminUserPage.jsx
+++ src/admin/AdminUserPage.jsx
@@ -3,4 +3,5 @@ import React from 'react'
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 // Modal for viewing user history
+const UserHistoryModal = ({ show, user, onClose }) => (
 
