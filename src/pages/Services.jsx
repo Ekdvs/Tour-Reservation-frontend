061186203aef96344eb29af887a39f6f9f3cfa53
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-05 10:51:07 +0530

    add service icon to Hotel Reservation section for enhanced visual appeal

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -46,7 +46,12 @@ export default function Services() {
                                         <h5 class="mb-4">Hotel Reservation</h5>
                                         <p class="mb-0">Dolor sit amet consectetur adipisicing elit. Non alias eum, suscipit expedita corrupti officiis debitis possimus nam laudantium beatae quidem dolore consequuntur voluptate rem reiciendis, omnis sequi harum earum.
                                         </p>
+                                        </div>
+                                        <div class="service-icon p-4">
+                                        <i class="fa fa-hotel fa-4x text-primary"></i>
                                     </div>
+                                </div>
+                            </div>
         <Footer/>
         </div>
     )
