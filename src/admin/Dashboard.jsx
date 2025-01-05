c6f1e0971b5a9c5a19212ce30fc979eb4e1e9b50
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-05 21:52:41 +0530

    add Sidebar and Nav components to Dashboard

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import Nav from './Nav'
 
 export default function Dashboard() {
   return (
     <div>
       <Sidebar/>
+      <Nav/>
 
     </div>
   )
