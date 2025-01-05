e18c71e4e40ef32abe8252946b745bbad9ba10bf
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-05 10:47:20 +0530

    add breadcrumb navigation to Services page for improved user context

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -11,7 +11,11 @@ export default function Services() {
             <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style={{ maxWidth: '900px' }}>
                 <h3 class="text-white display-3 mb-4">Our Services</h3>
-                
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/Services">Pages</a></li>
+                    <li class="breadcrumb-item active text-white">Services</li>
+                </ol>
         <Footer/>
         </div>
     )
