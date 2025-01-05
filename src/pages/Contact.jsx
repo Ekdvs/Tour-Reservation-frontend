a1b324693cf48bbe33ea4ff1cf89b4e90d067c86
Ekdvs <ekdvsampath02@gmail.com>
2025-01-05 08:57:18 +0530

    feat: add Topbar component to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Topbar from "../compodent/Topbar";
 
 export default function Contact() {
     return (
         <div>
-            
+            <Topbar/>
         </div>
     )
 }
