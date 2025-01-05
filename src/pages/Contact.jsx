41d6421b018ce8b66b83be8da6ba730054ce6b54
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-05 10:18:46 +0530

    add breadcrumb navigation to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,11 @@ export default function Contact() {
             <div class="container-fluid bg-breadcrumb">
             <div class="container text-center py-5" style={{ maxWidth: "900px" }}>
                 <h3 class="text-white display-3 mb-4">Contact Us</h3>
-                
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/Contact">Pages</a></li>
+                    <li class="breadcrumb-item active text-white">Contact</li>
+                </ol>
             <Footer/>
         </div>
     )
