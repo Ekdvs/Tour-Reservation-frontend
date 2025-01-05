06a8d085b0c08a01f7f711f02acb10953b2b57a6
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-05 21:54:50 +0530

    add header section to Dashboard layout

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -9,8 +9,11 @@ export default function Dashboard() {
       <Nav/>
       <div class="container">
         <div class="page-inner">
-        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
-
+          <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+            <div>
+                <h3 class="fw-bold mb-3">Ceylon Dashboard</h3>
+                <h6 class="op-7 mb-2"></h6>
+            </div>
 
 
     </div>
