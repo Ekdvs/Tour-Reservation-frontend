c9eb840ea2e5c3771a1fd6a50e50c5199a182acc
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-05 21:58:52 +0530

    add row div to card body in Dashboard layout

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -23,7 +23,7 @@ export default function Dashboard() {
             <div class="col-sm-6 col-md-3">
               <div class="card card-stats card-round">
                 <div class="card-body">
-
+                  <div class="row align-items-center">
 
     </div>
   )
