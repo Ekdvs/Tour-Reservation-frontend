c4c1e184a5e7b1edc84ca2ca27ef879de5159d69
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 21:00:33 +0530

    nav

--- src/admin/Nav.jsx
+++ src/admin/Nav.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Nav() {
   return (
     <div>
-      
+      <nav className="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
+
 
 
       
