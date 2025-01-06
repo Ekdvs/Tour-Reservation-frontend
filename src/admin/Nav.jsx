e338ba75b9124cfdbb52cfea843d4b1c3f1ad4ec
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 23:40:35 +0530

    add div

--- src/admin/Nav.jsx
+++ src/admin/Nav.jsx
@@ -206,6 +206,17 @@ export default function Nav() {
                       </a>
                       <a className="col-6 col-md-4 p-0" href="#">
                       <div className="quick-actions-item">
+                      <div className="avatar-item bg-info rounded-circle">
+                            <i className="fas fa-file-excel"></i>
+                          </div>
+
+
+
+
+
+
+
+
 
       
     </div>
