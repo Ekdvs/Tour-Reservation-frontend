cd98f0845d81e619a2455560919ebc997fdd3191
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 17:20:50 +0530

    th

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -239,6 +239,7 @@ export default function Dashboard() {
                             </td>
                           </tr>
                           <tr>
+                          <th scope="row">
 
 
 
