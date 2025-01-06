2cfbc7210e6e2e6e6ed60aaf60f4faf5fbedf852
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-07 00:14:06 +0530

    fetch all events

---
+++ src/admin/EventPage.jsx
@@ -0,0 +1,32 @@
+const EventManagement = () => {
+    const [events, setEvents] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [eventForm, setEventForm] = useState({
+      eventName: "",
+      eventDate: "",
+      eventTime: "",
+      eventVenue: "",
+      eventOrganizer: "",
+      description: "",
+      oneTicketPrice: "",
+      eventType: "",
+      eventIsFor: "",
+      numOfTickets: "",
+    });
+    const [eventImage, setEventImage] = useState(null);
+    const [editEvent, setEditEvent] = useState(null); // Store the event being edited
+  
+    // Fetch all events
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/event/getAllEvents");
+        setEvents(response.data);
+      } catch (error) {
+        toast.error("Error fetching events!");
+      }
+    };
+  
+    useEffect(() => {
+      fetchEvents();
+    }, []);
+  
