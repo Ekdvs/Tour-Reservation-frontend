3977292dab2533f7b7d8cf6e9ecfe7e1937da74d
Ekdvs <ekdvsampath02@gmail.com>
2025-01-06 11:14:34 +0530

    fix: add inline style to paragraph for improved text visibility in Services component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -128,7 +128,7 @@ export default function Services() {
                       </div>
                       <div class="service-content">
                         <h5 class="mb-4">Hotel Reservation</h5>
-                        <p class="mb-0">
+                        <p class="mb-0"style={{ color: "black" }}>
                           Dolor sit amet consectetur adipisicing elit. Non alias
                           eum, suscipit expedita corrupti officiis debitis
                           possimus nam laudantium beatae quidem dolore
