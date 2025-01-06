4229068ad4688b46b89f0b6d7196d15c734f7e21
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-07 01:09:31 +0530

    add div

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div>
-      
+      <Header/>
+    <div className="sidebar" data-background-color="dark">
+      <div className="sidebar-logo">
+       
+      </div>
+      <div className="sidebar-wrapper scrollbar scrollbar-inner">
+        <div className="sidebar-content">
+
     </div>
   )
 }
