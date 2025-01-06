e7df077ab498d496d165853087cbfecff7c6db10
Ekdvs <ekdvsampath02@gmail.com>
2025-01-06 11:13:41 +0530

    fix: replace h6 with p tag for better semantic structure in Services component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -36,13 +36,13 @@ export default function Services() {
                     <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
                       <div class="service-content text-end">
                         <h5 class="mb-4">WorldWide Tours</h5>
-                        <h6 class="mb-0">
+                        <p class="mb-0" style={{ color: "black" }}>
                           Dolor sit amet consectetur adipisicing elit. Non alias
                           eum, suscipit expedita corrupti officiis debitis
                           possimus nam laudantium beatae quidem dolore
                           consequuntur voluptate rem reiciendis, omnis sequi
                           harum earum.
-                        </h6>
+                        </p>
                       </div>
                       <div class="service-icon p-4">
                         <i class="fa fa-globe fa-4x text-primary"></i>
