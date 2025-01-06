6f9565d9a28cc0259a554dbb5d144fa1205d5fb8
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 23:54:13 +0530

    add li

--- src/admin/Nav.jsx
+++ src/admin/Nav.jsx
@@ -264,6 +264,30 @@ export default function Nav() {
                           className="avatar-img rounded"
                         />
                       </div>
+                      <div className="u-text">
+                        <h4>Hizrian</h4>
+                        <p className="text-muted">hello@example.com</p>
+                        <a href="profile.html" className="btn btn-xs btn-secondary btn-sm">View Profile</a>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">My Profile</a>
+                    <a className="dropdown-item" href="#">My Balance</a>
+                    <a className="dropdown-item" href="#">Inbox</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Account Setting</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Logout</a>
+                  </li>
+                </div>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
 
 
 
