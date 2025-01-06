9d62484058f039b6636d47b5ca1e7b010e1f44d9
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-07 01:07:21 +0530

    end

--- src/admin/Place.jsx
+++ src/admin/Place.jsx
@@ -1,3 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Places = () => {
     const [places, setPlaces] = useState([]);
     const [filteredPlaces, setFilteredPlaces] = useState([]);
