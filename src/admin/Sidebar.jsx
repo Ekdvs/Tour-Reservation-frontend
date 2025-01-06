460ad899685cb66c7169c3667df0d88e2f907268
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-07 01:10:02 +0530

    add ul

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -11,6 +11,33 @@ export default function Sidebar() {
       <div className="sidebar-wrapper scrollbar scrollbar-inner">
         <div className="sidebar-content">
 
+        <ul className="nav nav-secondary">
+            <li className="nav-item active">
+              <a
+                data-bs-toggle="collapse"
+                href="#dashboard"
+                className="collapsed"
+                aria-expanded="false"
+              >
+                <i className="fas fa-home"></i>
+                <p>Dashboard</p>
+                <span className="caret"></span>
+              </a>
+              <div className="collapse" id="dashboard">
+                <ul className="nav nav-collapse">
+                  <li>
+                    <a href="../demo1/index.html">
+                      <span className="sub-item">Dashboard 1</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+
+
+
+
+
     </div>
   )
 }
