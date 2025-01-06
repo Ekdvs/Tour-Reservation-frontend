9ed84e6137b81ad63c27ac6c83273a3a5c26c0bf
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 21:00:54 +0530

    add div

--- src/admin/Nav.jsx
+++ src/admin/Nav.jsx
@@ -4,6 +4,7 @@ export default function Nav() {
   return (
     <div>
       <nav className="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
+      <div className="container-fluid">
 
 
 
