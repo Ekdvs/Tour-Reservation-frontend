fcefd1112c8e1cafed3df1f28c5a03ee50df96f7
BAGMN <neshama-ps20138@stu.kln.ac.lk>
2025-01-06 17:18:56 +0530

    currency

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -233,7 +233,7 @@ export default function Dashboard() {
                               Payment from #10231
                             </th>
                             <td class="text-end">Mar 19, 2020, 2.45pm</td>
-
+                            <td class="text-end">$250.00</td>
 
 
 
