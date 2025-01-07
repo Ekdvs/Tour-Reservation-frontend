25d5ea92a2871b93c9e8042dc29e8c429870e494
Ekdvs <ekdvsampath02@gmail.com>
2025-01-07 09:38:12 +0530

    fix: correct indentation for link tag in Headeradmin component

--- src/compodent/Headeradmin.jsx
+++ src/compodent/Headeradmin.jsx
@@ -4,7 +4,7 @@ export default function Headeradmin() {
   return (
     <div>
       <head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="stylesheet" href="admin/assets/css/bootstrap.min.css" />
             <link rel="stylesheet" href="admin/assets/css/plugins.min.css" />
             <link rel="stylesheet" href="admin/assets/css/kaiadmin.min.css" />
