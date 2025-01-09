5e53b70a9fb38b5333901a69c95c54b2d9147208
Ekdvs <ekdvsampath02@gmail.com>
2025-01-09 11:28:01 +0530

    feat: add search input for User ID in ReservationUser component

--- src/pages/ReservationUser.jsx
+++ src/pages/ReservationUser.jsx
@@ -33,6 +33,22 @@ export default function ReservationUser() {
     <div>
         <div className="container mt-5">
         <h1 className="text-center mb-4">User Reservations</h1>
+        <div className="mb-4">
+        <h3>Search Reservations</h3>
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter User ID"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+
+          />
+           <button className="btn btn-primary" onClick={fetchReservationsByUserId}>
+            Fetch Reservations
+          </button>
+        </div>
+      </div>
       
     </div>
   )
