d46ce063a008c58695f06b0f7bc20ba531cb77ac
Ekdvs <ekdvsampath02@gmail.com>
2025-01-09 13:21:15 +0530

    feat: add Bootstrap CSS for improved styling in the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Register from "./user/Register";
 import SignIn from "./user/SignIn";
 import ForgotPassword from './user/ForgotPassword';
