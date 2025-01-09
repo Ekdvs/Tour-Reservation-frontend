5fa86ed6ceb7186d198288b49ea01ef67ce64c2f
Ekdvs <ekdvsampath02@gmail.com>
2025-01-09 11:46:02 +0530

    feat: initialize state for events in Cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Cart() {
+    const [events, setEvents] = useState([]);
   return (
     <div>
       
