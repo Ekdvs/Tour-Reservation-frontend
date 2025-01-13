b3e222ad531306dbd961fa1480b4566995f84910
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-13 13:00:07 +0530

    update gallery title to "Capture the Essence of Sri Lanka"

--- src/pages/Our_Gallery.jsx
+++ src/pages/Our_Gallery.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Our_Gallery() {
   return (
     <div>
-      <h1>Vishwa Vishwa </h1>
+      <h1>Capture the Essence of Sri Lanka</h1>
     </div>
-  )
+  );
 }
