d473f17a780882ff8adf3abf4c5527242a303e86
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-14 15:35:53 +0530

    refactor Services component for improved content clarity and layout

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -46,35 +46,38 @@ export default function Services() {
                           Start your journey today with a package that fits your
                           style.
                         </p>
+                        <b>Explore Our Packages</b>
                       </div>
                     </div>
                   </div>
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center  bg-white border border-primary rounded p-4 pe-0">
-                      <div class="service-content text-end">
-                        <h5 class="mb-4">Hotel Reservation</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Comfortable & Cozy Stays</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Find your perfect home away from home with our range
+                          of accommodations. Choose from luxurious hotels,
+                          relaxing resorts, and unique stays, all designed to
+                          make your trip unforgettable.
                         </p>
+                        <b>Book Your Stay Now</b>
                       </div>
                     </div>
                   </div>
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 pe-0">
-                      <div class="service-content text-end">
-                        <h5 class="mb-4">Travel Guides</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Seamless Travel Solutions</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Find your perfect home away from home with our range
+                          of accommodations. Choose from luxurious hotels,
+                          relaxing resorts, and unique stays, all designed to
+                          make your trip unforgettable.
                         </p>
+                        <b>Book Your Stay Now</b>
                       </div>
+                    </div>
+                  </div>
                       <div class="service-icon p-4">
                         <i class="fa fa-user fa-4x text-primary"></i>
                       </div>
