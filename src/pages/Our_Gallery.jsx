7b1e61ce2361c459cb6c9a2d7fc0389604604a93
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-14 09:56:27 +0530

    add Topbar, Navbar, and Footer components to Our_Gallery

--- src/pages/Our_Gallery.jsx
+++ src/pages/Our_Gallery.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
+import Topbar from "../compodent/Topbar";
+import Navbar from "../compodent/Navbar";
+import Footer from "../compodent/Footer";
+
 
 export default function Our_Gallery() {
   return (
