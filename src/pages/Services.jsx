9fbf8476be26581c7fff6508cfefcd7496dcf5f6
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-14 16:31:55 +0530

    update Services component content for improved clarity and engagement

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -96,70 +96,58 @@ export default function Services() {
               <div class="col-lg-6">
                 <div class="row g-4">
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 ps-0">
-                      <div class="service-icon p-4">
-                        <i class="fa fa-globe fa-4x text-primary"></i>
-                      </div>
-                      <div class="service-content">
-                        <h5 class="mb-4">WorldWide Tours</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Seamless Travel Solutions</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Enjoy smooth, stress-free journeys with our
+                          transportation services. From airport transfers to
+                          local travel, we ensure you get to your destination
+                          comfortably and on time.
                         </p>
+                        <b>Discover More</b>
                       </div>
                     </div>
                   </div>
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 ps-0">
-                      <div class="service-icon p-4">
-                        <i class="fa fa-hotel fa-4x text-primary"></i>
-                      </div>
-                      <div class="service-content">
-                        <h5 class="mb-4">Hotel Reservation</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Seamless Travel Solutions</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Enjoy smooth, stress-free journeys with our
+                          transportation services. From airport transfers to
+                          local travel, we ensure you get to your destination
+                          comfortably and on time.
                         </p>
+                        <b>Discover More</b>
                       </div>
                     </div>
                   </div>
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 ps-0">
-                      <div class="service-icon p-4">
-                        <i class="fa fa-user fa-4x text-primary"></i>
-                      </div>
-                      <div class="service-content">
-                        <h5 class="mb-4">Travel Guides</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Seamless Travel Solutions</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Enjoy smooth, stress-free journeys with our
+                          transportation services. From airport transfers to
+                          local travel, we ensure you get to your destination
+                          comfortably and on time.
                         </p>
+                        <b>Discover More</b>
                       </div>
                     </div>
                   </div>
                   <div class="col-12">
-                    <div class="service-content-inner d-flex align-items-center bg-white border border-primary rounded p-4 ps-0">
-                      <div class="service-icon p-4">
-                        <i class="fa fa-cog fa-4x text-primary"></i>
-                      </div>
-                      <div class="service-content">
-                        <h5 class="mb-4">Event Management</h5>
-                        <p class="mb-0" style={{ color: "black" }}>
-                          Dolor sit amet consectetur adipisicing elit. Non alias
-                          eum, suscipit expedita corrupti officiis debitis
-                          possimus nam laudantium beatae quidem dolore
-                          consequuntur voluptate rem reiciendis, omnis sequi
-                          harum earum.
+                    <div class="service-content-inner d-flex  bg-white border border-primary rounded p-4 pe-4">
+                      <div>
+                        <h5>Seamless Travel Solutions</h5>
+                        <p className="text-justify" style={{ color: "black" }}>
+                          Enjoy smooth, stress-free journeys with our
+                          transportation services. From airport transfers to
+                          local travel, we ensure you get to your destination
+                          comfortably and on time.
                         </p>
+                        <b>Discover More</b>
                       </div>
                     </div>
                   </div>
