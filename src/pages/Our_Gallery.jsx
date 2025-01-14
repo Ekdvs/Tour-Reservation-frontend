d000a51e80b91476c5f81160f7dbe3eadc6744ee
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-14 09:45:46 +0530

    add Bootstrap for styling in Our_Gallery component

--- src/pages/Our_Gallery.jsx
+++ src/pages/Our_Gallery.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 export default function Our_Gallery() {
   return (
