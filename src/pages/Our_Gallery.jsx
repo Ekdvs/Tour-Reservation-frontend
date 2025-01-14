ce262e49fc9ae87a68e97b475072c488c42e0446
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-14 09:57:32 +0530

    add image paths from the public folder in Our_Gallery component

--- src/pages/Our_Gallery.jsx
+++ src/pages/Our_Gallery.jsx
@@ -5,8 +5,9 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import Topbar from "../compodent/Topbar";
 import Navbar from "../compodent/Navbar";
 import Footer from "../compodent/Footer";
-
-
+// Image paths from the public folder
+const images = [
+  "/img/gallery-1.jpg",
 export default function Our_Gallery() {
   return (
     <div>
