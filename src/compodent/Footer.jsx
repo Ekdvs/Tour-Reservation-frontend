04c70a968537ecef2a4418c0790ae0b428646937
Ekdvs <ekdvsampath02@gmail.com>
2025-01-15 19:12:29 +0530

    add email

--- src/compodent/Footer.jsx
+++ src/compodent/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
                         <div className="footer-item d-flex flex-column">
                             <h4 className="mb-4 text-white">Get In Touch</h4>
                             <a href=""><i className="fas fa-home me-2"></i> 123 Kandy Colombo Main Street, Kelaniya, Sri Lanka</a>
-                            <a href=""><i className="fas fa-envelope me-2"></i> info@example.com</a>
+                            <a href=""><i className="fas fa-envelope me-2"></i> ceylontravelplanning@gmail.com</a>
                             <a href=""><i className="fas fa-phone me-2"></i> +9471 897 4153</a>
                             <a href="" className="mb-3"><i className="fas fa-print me-2"></i> +9477 326 0660</a>
                             <div className="d-flex align-items-center">
