4a672fc6147a08a43628c30913c9bf26bb6dfd2f
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-30 10:07:20 +0530

    add package form state initialization in PackageManagement component

--- src/admin/PackagePage.jsx
+++ src/admin/PackagePage.jsx
@@ -7,4 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const PackageManagement = () => {
     const [packages, setPackages] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
-  const [packageForm, setPackageForm] = useState({
+    const [packageForm, setPackageForm] = useState({
+      packageName: "",
+    packageType: "",
+    description: "",
+    onePersonPrice: "",
+    duration: "",
+    location: "",
+  });
