b5d13a096c3fd068607c1d027291ead8b9ffea41
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-30 10:05:21 +0530

    import Bootstrap CSS for improved styling in PackagePage component

--- src/admin/PackagePage.jsx
+++ src/admin/PackagePage.jsx
@@ -2,3 +2,4 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
