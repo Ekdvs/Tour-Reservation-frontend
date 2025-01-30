875f90e13add7b8f3b60bd81bb4e66be0c690247
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-01-30 10:25:55 +0530

    add package details rendering to PackagePage component

--- src/admin/PackagePage.jsx
+++ src/admin/PackagePage.jsx
@@ -226,4 +226,8 @@ const PackageManagement = () => {
           <li
             key={pkg.id}
             className="list-group-item d-flex justify-content-between align-items-center"
-          ></li>
+                    >
+                    <div>
+              <h5>{pkg.packageName}</h5>
+              <p>{pkg.description}</p>
+            </div>
