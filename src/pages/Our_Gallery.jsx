7f06508b105f7cce64cfa8f7460e34862946f53d
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-02-03 08:50:07 +0530

    style: add border radius and top border to Our_Gallery component

--- src/pages/Our_Gallery.jsx
+++ src/pages/Our_Gallery.jsx
@@ -57,6 +57,8 @@ export default function Our_Gallery() {
           backgroundImage: `linear-gradient(rgba(19, 53, 123, .6), rgba(19, 53, 123, .6)), url(../img/R.jpeg)`,
           backgroundSize: "cover",
           background_attachment: "fixed",
+          borderRadius: "15px",
+          borderTop: "2px solid #fff",
         }}
       >
         {/* Carousel Section */}
