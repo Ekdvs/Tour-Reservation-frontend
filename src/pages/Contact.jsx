738ffa69f697ea611107ae23964551da70e41167
Ishara Jayawardhana <ijjayawardhana46367@gmail.com>
2025-02-03 08:52:31 +0530

    style: add border radius and top border to Contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -52,6 +52,8 @@ export default function Contact() {
           backgroundImage: `linear-gradient(rgba(19, 53, 123, .6), rgba(19, 53, 123, .6)), url(../img/R.jpeg)`,
           backgroundSize: "cover",
           background_attachment: "fixed",
+          borderRadius: "15px",
+          borderTop: "2px solid #fff",
         }}
       >
         <div className="container py-5">
