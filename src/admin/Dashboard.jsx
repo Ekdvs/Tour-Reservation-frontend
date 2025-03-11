5fe3018aa301226f53788f3577dac2f539ebebe4
Ekdvs <ekdvsampath02@gmail.com>
2025-03-11 12:35:15 +0530

    fix alll

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,252 +1,70 @@
-import React, { useEffect, useState } from "react";
-import Sidebar from "./Sidebar";
-import Nav from "./Nav";
-import OnlineUsersCard from "./OnlineUserCard";
-import NewCustomers from "./NewCustomer";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import Sidebar from './Sidebar';
+import Nav from './Nav';
+import OnlineUsersCard from './OnlineUserCard';
+import NewCustomers from './NewCustomer';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 export default function Dashboard() {
   return (
     <div>
       <Sidebar />
       <Nav />
-      <div class="container">
-        <div class="page-inner">
-          <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
+      <div className="container">
+        <div className="page-inner">
+          <div className="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
             <div>
-              <h3 class="fw-bold mb-3">Ceylon Dashboard</h3>
-              <h6 class="op-7 mb-2"></h6>
+              <h3 className="fw-bold mb-3">Ceylon Dashboard</h3>
             </div>
-            <div class="ms-md-auto py-2 py-md-0">
-              <a href="manage" class="btn btn-label-info btn-round me-2">
+            <div className="ms-md-auto py-2 py-md-0">
+              <a href="manage" className="btn btn-label-info btn-round me-2">
                 Manage
               </a>
-              <a href="addtravelguide" class="btn btn-primary btn-round">
+              <a href="addtravelguide" className="btn btn-primary btn-round">
                 Add Travel Guide
               </a>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-6 col-md-3">
-              <div class="card card-stats card-round">
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col-icon">
-                      <div class="icon-big text-center icon-primary bubble-shadow-small">
-                        <i class="fas fa-users"></i>
+          <div className="row">
+            <div className="col-sm-6 col-md-3">
+              <div className="card card-stats card-round">
+                <div className="card-body">
+                  <div className="row align-items-center">
+                    <div className="col-icon">
+                      <div className="icon-big text-center icon-primary bubble-shadow-small">
+                        <i className="fas fa-users"></i>
                       </div>
                     </div>
-                    <div class="col col-stats ms-3 ms-sm-0">
-                      <div class="numbers">
-                        <p class="card-category">Visitors</p>
-                        <h4 class="card-title">1,294</h4>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-              <div class="card card-stats card-round">
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col-icon">
-                      <div class="icon-big text-center icon-info bubble-shadow-small">
-                        <i class="fas fa-user-check"></i>
-                      </div>
-                    </div>
-                    <div class="col col-stats ms-3 ms-sm-0">
-                      <div class="numbers">
-                        <p class="card-category">Subscribers</p>
-                        <h4 class="card-title">1303</h4>
+                    <div className="col col-stats ms-3 ms-sm-0">
+                      <div className="numbers">
+                        <p className="card-category">Visitors</p>
+                        <h4 className="card-title">1,294</h4>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-sm-6 col-md-3">
-              <div class="card card-stats card-round">
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col-icon">
-                      <div class="icon-big text-center icon-success bubble-shadow-small">
-                        <i class="fas fa-luggage-cart"></i>
-                      </div>
-                    </div>
-                    <div class="col col-stats ms-3 ms-sm-0">
-                      <div class="numbers">
-                        <p class="card-category">Sales</p>
-                        <h4 class="card-title">$ 1,345</h4>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-3">
-              <div class="card card-stats card-round">
-                <div class="card-body">
-                  <div class="row align-items-center">
-                    <div class="col-icon">
-                      <div class="icon-big text-center icon-secondary bubble-shadow-small">
-                        <i class="far fa-check-circle"></i>
-                      </div>
-                    </div>
-                    <div class="col col-stats ms-3 ms-sm-0">
-                      <div class="numbers">
-                        <p class="card-category">Order</p>
-                        <h4 class="card-title">576</h4>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {/* Add more cards */}
           </div>
-          <div class="row">
-            <div class="col-md-8">
-              <div class="card card-round">
-                <div class="card-header">
-                  <div class="card-head-row">
-                    <div class="card-title">User Statistics</div>
-                    <div class="card-tools">
-                      <a
-                        href="#"
-                        class="btn btn-label-success btn-round btn-sm me-2"
-                      >
-                        <span class="btn-label">
-                          <i class="fa fa-pencil"></i>
-                        </span>
-                        Export
-                      </a>
-                      <a href="#" class="btn btn-label-info btn-round btn-sm">
-                        <span class="btn-label">
-                          <i class="fa fa-print"></i>
-                        </span>
-                        Print
-                      </a>
-                    </div>
+          <div className="row">
+            <div className="col-md-8">
+              <div className="card card-round">
+                <div className="card-header">
+                  <div className="card-head-row">
+                    <div className="card-title">User Statistics</div>
                   </div>
                 </div>
-                <div class="card-body">
-                  <div class="chart-container" style={{ minHeight: "375px" }}>
+                <div className="card-body">
+                  <div className="chart-container" style={{ minHeight: '375px' }}>
                     <canvas id="statisticsChart"></canvas>
                   </div>
-                  <div id="myChartLegend"></div>
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="card card-primary card-round">
-                <div class="card-header">
-                  <div class="card-head-row">
-                    <div class="card-title">Daily Sales</div>
-                    <div class="card-tools">
-                      <div class="dropdown">
-                        <button
-                          class="btn btn-sm btn-label-light dropdown-toggle"
-                          type="button"
-                          id="dropdownMenuButton"
-                          data-bs-toggle="dropdown"
-                          aria-haspopup="true"
-                          aria-expanded="false"
-                        >
-                          Export
-                        </button>
-                        <div
-                          class="dropdown-menu"
-                          aria-labelledby="dropdownMenuButton"
-                        >
-                          <a class="dropdown-item" href="#">
-                            Action
-                          </a>
-                          <a class="dropdown-item" href="#">
-                            Another action
-                          </a>
-                          <a class="dropdown-item" href="#">
-                            Something else here
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-category">March 25 - April 02</div>
-                </div>
-                <div class="card-body pb-0">
-                  <div class="mb-4 mt-2">
-                    <h1>$4,578.58</h1>
-                  </div>
-                  <div class="pull-in">
-                    <canvas id="dailySalesChart"></canvas>
-                  </div>
-                </div>
-              </div>
+            <div className="col-md-4">
               <OnlineUsersCard />
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-4">
               <NewCustomers />
-            </div>
-          </div>
-
-          <div class="col-md-8">
-            <div class="card card-round">
-              <div class="card-header">
-                <div class="card-head-row card-tools-still-right">
-                  <div class="card-title">Transaction History</div>
-                  <div class="card-tools">
-                    <div class="dropdown">
-                      <button
-                        class="btn btn-icon btn-clean me-0"
-                        type="button"
-                        id="dropdownMenuButton"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        <i class="fas fa-ellipsis-h"></i>
-                      </button>
-                      <div
-                        class="dropdown-menu"
-                        aria-labelledby="dropdownMenuButton"
-                      >
-                        <a class="dropdown-item" href="#">
-                          Action
-                        </a>
-                        <a class="dropdown-item" href="#">
-                          Another action
-                        </a>
-                        <a class="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body p-0">
-                <div class="table-responsive">
-                  <table class="table align-items-center mb-0">
-                    <thead class="thead-light">
-                      <tr>
-                        <th scope="col">Payment Number</th>
-                        <th scope="col" class="text-end">
-                          Date & Time
-                        </th>
-                        <th scope="col" class="text-end">
-                          Amount
-                        </th>
-                        <th scope="col" class="text-end">
-                          Status
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody></tbody>
-                  </table>
-                </div>
-              </div>
             </div>
           </div>
         </div>
